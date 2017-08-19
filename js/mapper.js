82b809d72fe05595e55ec444a8594b13d2acb89f
Thomas Rea <thomasmarkrea@gmail.com>
2017-08-19 12:25:13 +0100

    add functions to colour and update map

--- js/mapper.js
+++ js/mapper.js
@@ -1,6 +1,3 @@
-// TODO: use d3.dispatch to handle events?
-//       https://bl.ocks.org/mbostock/5872848
-
 // load data
 d3.queue()
 	.defer(d3.json, 'data/countries_topo.json')
@@ -16,10 +13,8 @@ dispatch.on('update', update);
 
 function startApp(error, countries, market_data) {
   if(error) throw error;
-  // select(market_data);
-  // map(countries);
   dispatch.call('draw_options', this, market_data);
-  // dispatch.call('draw_map', this, countries);
+  dispatch.call('draw_map', this, countries);
 }
 
 function drawOptions(data) {
@@ -35,7 +30,7 @@ function drawOptions(data) {
     var select = d3.select('body')
       .append('select')
         .attr('id', key)
-        .on('change', changeEvent);
+        .on('change', function(){ dispatch.call('update', this, data_map); });
 
     select.selectAll()
       .data(attribute_values.values())
@@ -44,37 +39,9 @@ function drawOptions(data) {
         .property('value', function(d){ return d; })
         .text(function(d){ return d; });
   });
-
-  function changeEvent() {
-    console.log(this);
-    console.log(event);
-
-    var field = event.srcElement.id;
-    var target = event.target.value;
-
-    console.log(field);
-    console.log(target);
-
-    clear_map();
-    id_countries(data, target, key);
-
-    // companies.forEach(function(company, i){
-    //   if(company.name == event.target.value){
-    //     company.markets.forEach(function(market, i){
-    //       var selector = '.'+market;
-    //       svg.selectAll(selector)
-    //         .style('fill', '#009cde');
-    //     });
-    //   }
-    // });
-  }
 }
 
 function set(data, type, key) {
-  console.log(data);
-  console.log(type);
-  console.log(key);
-
   var set = d3.set();
 
   data.each(function(value) {
@@ -87,30 +54,42 @@ function set(data, type, key) {
     });
   });
 
-  console.log(set);
   return set;
 }
 
 function fetchAttributeKeys(value) {
-  console.log(value);
-
   var attributes = value.attributes;
   var keys = d3.keys(attributes);
   var attribute_keys = d3.set(keys);
 
-  console.log(attribute_keys);
   return attribute_keys;
 }
 
 function fetchAttributeValues(value, key) {
-  console.log(value);
-  console.log(key);
-
   var attributes = value.attributes;
   var attribute_values = d3.set(attributes[key]);
 
-  console.log(attribute_values);
   return attribute_values;
+}
+
+function update(data) {
+  var field = event.srcElement.id;
+  var value = event.target.value;
+  var countries = id_countries(data, field, value);
+
+  clear_map();
+  colour_map(countries);
+}
+
+function id_countries(data, field, value) {
+  countries = d3.set();
+
+  data.each(function(obj) {
+    var set = d3.set(obj.attributes[field]);
+    if(set.has(value)) countries.add(obj.iso_code);
+  });
+
+  return countries;
 }
 
 function clear_map() {
@@ -120,16 +99,14 @@ function clear_map() {
     .style('fill', '#000');
 }
 
-function id_countries(data, target, key) {
-  countries = d3.set();
+function colour_map(countries) {
+  var svg = d3.select('svg');
 
-  data.each(function(value) {
-    var set = d3.set(data[key]);
-    if(set.has(target)) {
-
-      countries.add()
-    }
-  })
+  countries.each(function(country) {
+    var selector = '.'+country;
+    svg.selectAll(selector)
+      .style('fill', '#009cde');
+  });
 }
 
 function drawMap(countries) {
